07cb9af09d6c849430593c0364c553ea662b51b7
Betoluchenco <63628143+Betoluchenco@users.noreply.github.com>
2025-07-02 18:59:05 +0000

    semaforo

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-//Bootstrap
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+import "../styles/index.css";
+import "../styles/trafficLight.css";
+import TrafficLight from "./components/TrafficLight";
 
-// index.css'
-import '../styles/index.css'
-
-// components
-import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <TrafficLight />
+  </React.StrictMode>
+);
